28ac725cc7ff6f3e5d7045a6c0c786c61b858049
alysetran <66258581+alysetran@users.noreply.github.com>
2021-02-23 09:45:41 -0800

    direct copy of commands professor ran

---
+++ ImageHW1.m
@@ -0,0 +1,35 @@
+IO = imread('/Users/circuiteer/IPNDV-projects/Homework 1/cezanne 1.jpg');
+I = rgb2gray(IO);
+I1 = IO;
+FI1 = fft2(I1);
+I1 = I;
+FI1 = fft2(I1);
+[x_sz, y_sz] = size(I1);
+f1t = zeros(x_sz, y_sz);
+f1t = random('Uniform', 0, 128, x_sz, y_sz);
+f1t = floor(f1t);
+Ff1t = fft2(f1t);
+FI2 = FI1 .* Ff1t;
+FI3 = FI2 ./ Ff1t;
+I3 = ifft2(FI3);
+imshow(I3)
+
+
+f1t = random('Uniform', -8, 8, x_sz, y_sz);
+Ff1t = fft2(f1t);
+FI2 = FI1 .* Ff1t;
+FI3 = FI2 ./ Ff1t;
+I3 = ifft2(FI3);
+imshow(I3)
+FI3 = FI2./Ff1t;
+I3 = ifft2(FI3);
+imshow(uint8(I3));
+IO = imread('/Users/circuiteer/IPNDV-projects/Homework 1/cezanne 1.jpg');
+I = rgb2gray(IO);
+imshow(I);
+f1t = random('Uniform', 0, 128, x_sz, y_sz);
+f1t = floor(f1t);
+Ff1t = fft2(f1t);
+FI2 = FI1 .* Ff1t;
+FI3 = FI2 ./ Ff1t;
+imshow(FI3)
