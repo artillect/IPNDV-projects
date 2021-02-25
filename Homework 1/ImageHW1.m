258a339ac9273f2916ad9111676c5c88c40d23b2
Riley <oreo4455@gmail.com>
2021-02-25 11:35:08 -0500

    Trying something new

--- Homework 1/ImageHW1.m
+++ Homework 1/ImageHW1.m
@@ -1,4 +1,4 @@
-IO = imread('/Users/circuiteer/IPNDV-projects/Homework 1/cezanne 1.jpg');
+IO = imread('cezanne 1.jpg');
 I = rgb2gray(IO);
 I1 = IO;
 FI1 = fft2(I1);
@@ -24,7 +24,7 @@ imshow(I3)
 FI3 = FI2./Ff1t;
 I3 = ifft2(FI3);
 imshow(uint8(I3));
-IO = imread('/Users/circuiteer/IPNDV-projects/Homework 1/cezanne 1.jpg');
+IO = imread('cezanne 1.jpg');
 I = rgb2gray(IO);
 imshow(I);
 f1t = random('Uniform', 0, 128, x_sz, y_sz);
--- Homework 1/riley.m
+++ Homework 1/riley.m
@@ -1,22 +1,23 @@
 % Read image and make it grayscale
-I0 = imread('ground truth 1.jpg');
+I0 = imread('cezanne 1.jpg');
 I0 = rgb2gray(I0);
-I1 = imread('cezanne 1.jpg');
+I1 = imread('ground truth 1.jpg');
 I1 = rgb2gray(I1);
 
-FT0 = fft(I0);
-FT1 = fft(I1);
+FT0 = fft2(I0);
+FT1 = fft2(I1);
 
 flt = FT0 ./ FT1;
 
 figure(1);
 imshow(flt);
 
-I0 = imread('ground truth 2.jpg');
+I0 = imread('cezanne 2.jpg');
 I0 = rgb2gray(I0);
-FT0 = fft(I0);
-FT1 = FT0 .* flt;
+FT0 = fft2(I0);
+FT1 = FT0 ./ flt;
 
 I1 = ifft(FT1);
 figure(2);
-imshow(I1);
+
+imshow(uint8(I1));
