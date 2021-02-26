087fa8dc0c26693427c91ee43c6e06a9bff7a043
Riley <oreo4455@gmail.com>
2021-02-25 19:44:36 -0500

    Added RGB

--- Homework 1/rgb.m
+++ Homework 1/rgb.m
@@ -1,42 +1,44 @@
 % Read images and make them grayscale
 IC1 = imread('cezanne 1.jpg');
-IC1 = im2double(rgb2gray(IC1));
+IC1 = im2double(IC1);
 IGT1 = imread('ground truth 1.jpg');
-IGT1 = im2double(rgb2gray(IGT1));
+IGT1 = im2double(IGT1);
 
-figure(1);
-imshow(IC1);
 
 IC2 = imread('cezanne 2.jpg');
-IC2 = im2double(rgb2gray(IC2));
+IC2 = im2double(IC2);
 IGT2 = imread('ground truth 2.jpg');
-IGT2 = im2double(rgb2gray(IGT2));
+IGT2 = im2double(IGT2);
 
 IC3 = imread('cezanne 3.jpg');
-IC3 = im2double(rgb2gray(IC3));
+IC3 = im2double(IC3);
 IGT3 = imread('ground truth 3.jpg');
-IGT3 = im2double(rgb2gray(IGT3));
+IGT3 = im2double(IGT3);
+
+
+figure(1);
+imshow(IC3);
 
 IC4 = imread('cezanne 4.jpg');
-IC4 = im2double(rgb2gray(IC4));
+IC4 = im2double(IC4);
 IGT4 = imread('ground truth 4.jpg');
-IGT4 = im2double(rgb2gray(IGT4));
+IGT4 = im2double(IGT4);
 
 % Take 2d fourier transforms of images
-FTC1 = fft2(fftshift(IC1));
-FTGT1 = fft2(fftshift(IGT1));
+FTC1 = fftn(fftshift(IC1));
+FTGT1 = fftn(fftshift(IGT1));
 
 figure(2);
 imshow(FTC1);
 
-FTC2 = fft2(fftshift(IC2));
-FTGT2 = fft2(fftshift(IGT2));
+FTC2 = fftn(fftshift(IC2));
+FTGT2 = fftn(fftshift(IGT2));
 
-FTC3 = fft2(fftshift(IC3));
-FTGT3 = fft2(fftshift(IGT3));
+FTC3 = fftn(fftshift(IC3));
+FTGT3 = fftn(fftshift(IGT3));
 
-FTC4 = fft2(fftshift(IC4));
-FTGT4 = fft2(fftshift(IGT4));
+FTC4 = fftn(fftshift(IC4));
+FTGT4 = fftn(fftshift(IGT4));
 
 % Determine filter for each image
 flt1 = FTC1 ./ FTGT1;
@@ -51,15 +53,15 @@ imshow(flt);
 
 % Try to recreate original image
 IC = imread('cezanne 3.jpg');
-IC = im2double(rgb2gray(IC));
-FTC = fft2(fftshift(IC));
+IC = im2double(IC);
+FTC = fftn(fftshift(IC));
 % Calculate the prediction of what the real image looked like
 FTR = FTC ./ flt;
 
-IR = ifftshift(ifft2(FTR));
+IR = ifftshift(ifftn(FTR));
 
 figure(4);
-imshow(fft2(fftshift(IR)));
+imshow(fftn(fftshift(IR)));
 
 figure(5);
 imshow(IR);
