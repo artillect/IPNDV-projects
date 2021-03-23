fe475e3d4096512bbbfcfb669bef953f492ad4ac
Riley <oreo4455@gmail.com>
2021-03-22 20:26:19 -0400

    Added distance-based intensity modification to blur objects

--- Homework 2/movecircle2modified.m
+++ Homework 2/movecircle2modified.m
@@ -2,14 +2,14 @@ fileName = 'movecircle.avi';
 frameRate = 5;
 movieLength = 20;
 
-num_obs = 16;
+num_obs = 32;
 vel = 32; % 4, 8, or 32 pixels/frame
 min_corr = 0.7;
 SNR = 20;
 
 x_array = randi([1 1024], num_obs, 1);
 y_array = randi([1 1024], num_obs, 1);
-r_array = 5 * rand(num_obs, 1);
+r_array = 2 * rand(num_obs, 1) + 1;
 dir_array = 2 * pi * rand(num_obs, 1);
 theta = linspace(0,2*pi,100);
 
@@ -28,8 +28,9 @@ for frameNo = 1 : frameRate * movieLength
         
         for x = round(x_coord - radius) : round(x_coord + radius)
             for y = round(y_coord - radius) : round(y_coord + radius)
-                if sqrt((x-x_coord)^2+(y-y_coord)^2) < radius && x < 1024 && x  > 1 && y < 1024 && y > 0
-                    im(x,y) = 1;
+                dist = sqrt((x - x_coord)^2 + (y - y_coord)^2);
+                if dist < radius && x < 1024 && x  > 1 && y < 1024 && y > 0
+                    im(x,y) = 1-dist/radius;
                 end
             end
         end
