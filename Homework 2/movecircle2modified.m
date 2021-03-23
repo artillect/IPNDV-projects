0e2452defa8f3043bf304a0f8d05d69693c826e8
Riley <oreo4455@gmail.com>
2021-03-22 21:47:13 -0400

    Got irregular shapes and blurring working in part 2

--- Homework 2/movecircle2modified.m
+++ Homework 2/movecircle2modified.m
@@ -3,14 +3,13 @@ frameRate = 5;
 movieLength = 20;
 
 num_obs = 32;
-vel = 32; % 4, 8, or 32 pixels/frame
+vel = 4; % 4, 8, or 32 pixels/frame
 min_corr = 0.7;
 SNR = 20;
 
 x_array = randi([1 1024], num_obs, 1);
 y_array = randi([1 1024], num_obs, 1);
-r_array = 2 * rand(num_obs, 1) + 1;
-dir_array = 2 * pi * rand(num_obs, 1);
+r_array = 2 * rand(num_obs, 1) + 2;
 theta = linspace(0,2*pi,100);
 
 
@@ -19,8 +18,8 @@ v.FrameRate = frameRate;
 open(v)
 
 for frameNo = 1 : frameRate * movieLength
-    im = 1/SNR * rand(1024, 1024); % generate background with noise with given SNR
-
+    im = zeros(1024, 1024);
+    
     for i = 1 : num_obs
         x_coord = x_array(i);
         y_coord = y_array(i);
@@ -29,8 +28,9 @@ for frameNo = 1 : frameRate * movieLength
         for x = round(x_coord - radius) : round(x_coord + radius)
             for y = round(y_coord - radius) : round(y_coord + radius)
                 dist = sqrt((x - x_coord)^2 + (y - y_coord)^2);
-                if dist < radius && x < 1024 && x  > 1 && y < 1024 && y > 0
-                    im(x,y) = 1-dist/radius;
+                if dist < radius * (0.3 * rand() + 0.7) && x < 1024 && x  > 1 && y < 1024 && y > 0
+                % if dist < radius && x < 1024 && x  > 1 && y < 1024 && y > 0
+                    im(x,y) = 1;
                 end
             end
         end
@@ -38,21 +38,17 @@ for frameNo = 1 : frameRate * movieLength
         x_coord = x_coord + vel * cos(rand_angle);
         y_coord = y_coord + vel * sin(rand_angle);
         
-        if round(x_coord) <= 1
-            x_coord = x_coord + 1023;
-        elseif round(x_coord) > 1024
-            x_coord = x_coord - 1023;
-        end
-        
-        if round(y_coord) <= 1
-            y_coord = y_coord + 1023;
-        elseif round(y_coord) > 1024
-            y_coord = y_coord - 1023;
-        end
-        
         x_array(i) = x_coord;
         y_array(i) = y_coord;
     end
+    
+    im = imgaussfilt(im, 0.875);
+    
+    noise = 1/SNR * rand(1024, 1024); % generate noise with given SNR
+    
+    im = im + noise;
+    
+    im(im > 1) = 1; % Set all intensities greater than 1 to 1
     
     writeVideo(v, im);
     imshow(im);
