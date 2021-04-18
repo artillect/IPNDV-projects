dc684c3b10f18214a3d8296c7a222f81474f6329
artillect <oreo4455@gmail.com>
2021-04-18 19:50:20 -0400

    Added file output with tracking data

--- Homework 3/tracking.m
+++ Homework 3/tracking.m
@@ -13,28 +13,41 @@ estimated_radius = 2;
 estimated_size = round(pi*estimated_radius^2);
 n_particles = 256;
 
+timestep = 0.2;
 % Autothreshold images
 
 
 % Set measurements for particle analysis
 IJ.run("Set Measurements...", "area center centroid redirect=None decimal=3");
 
+particleCoords = zeros(0, n_particles*2);
+
 % Track positions of particles from movie
-for i = 1:100
-    IJ.setSlice(i);
+for frame = 1:100
+    IJ.setSlice(frame);
     
     % Convert frame to grayscale
     IJ.run("8-bit");
     
     IJ.setThreshold(140, 255);
-    % Find positions of particles
-    IJ.run("Analyze Particles...", "size=0-" + estimated_size + " show=Nothing display clear slice");
+    % Find positions of particles 
+    % (wrapped in evalc to suppress command window output)
+    evalc('IJ.run("Analyze Particles...", "size=0-" + estimated_size + " show=Nothing display clear slice")');
     
-    % 
+    % Get results from ImageJ
     res = ResultsTable.getResultsTable();
     
-    for particleID = 1:(res.size() - 1)
-       x_m = res.getValue("XM", particleID);
-       y_m = res.getValue("YM", particleID);
+    numFound = res.size();
+
+    
+    % Get coordinates of particles in frame
+    for particleID = 1:n_particles
+        if particleID < res.size()
+            x_m = res.getValue("XM", particleID);
+            y_m = res.getValue("YM", particleID);
+            particleCoords = [particleCoords; particleID frame * timestep x_m y_m];
+        end
     end
+    
+    save('particlecoords.mat', 'particleCoords');
 end
